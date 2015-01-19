f6d1bce9d34bb3db34d7f50ec5838f04802270b6
Xiao Keting <peter.xiau@gmail.com>
2015-01-20 00:29:30 +0800

    fb 改惹，不動了。只好跟著改

--- ivereadthat/read.js
+++ ivereadthat/read.js
@@ -1,23 +1,21 @@
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver
 var observer = new MutationObserver(function(mutations, observer) {
-    jQuery(".UFILikeLink").each(function() {
-        if($(this).text() == "Like" || $(this).text() == "讚") {
-            $(this).text("朕知道了")
-        }
-        else if($(this).text() == "Unlike" || $(this).text() == "收回讚") {
-            $(this).text("朕也是看報紙才知道")
-        }
+
+    jQuery("[title='覺得這很讚']").each(function() {
+        $(this).text("朕知道了");
     });
 
-    jQuery(".uiLinkButtonInput").each(function() {
-        if ($(this).attr("value") == "Comment" || $(this).text("value") == "留言") {
-            $(this).attr("value", "卿家可知？")
-        }
+    jQuery("[title='收回讚']").each(function() {
+        $(this).text("朕也是看報紙才知道");
+    });
+
+    jQuery("[value='留言']").each(function() {
+        $(this).attr("value", "卿家可知？");
     });
 
     jQuery(".share_action_link").each(function() {
         if ($(this).text() == "Share" || $(this).text() == "分享") {
-            $(this).text("宣旨")
+            $(this).text("宣旨");
         }
     });
 })
