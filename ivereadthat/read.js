3b2c4007b3a24bc7dc3ed257ff3ae84bf28a88c3
Scott Wang <lunastorm@lunastorm.tw>
2015-01-20 10:30:47 +0800

    fix english compatibility

--- ivereadthat/read.js
+++ ivereadthat/read.js
@@ -1,23 +1,23 @@
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver
 var observer = new MutationObserver(function(mutations, observer) {
-
-    jQuery("[title='覺得這很讚']").each(function() {
-        $(this).text("朕知道了");
-    });
-
-    jQuery("[title='收回讚']").each(function() {
-        $(this).text("朕也是看報紙才知道");
-    });
-
-    jQuery("[value='留言']").each(function() {
-        $(this).attr("value", "卿家可知？");
-    });
-
+    jQuery(".UFILikeLink").each(function() {
+        if(this.title == "Like this" || this.title == "覺得這很讚") {
+            $(this).children("span").html("朕知道了")
+        }
+        else if(this.title == "Unlike this" || this.title == "收回讚") {
+            $(this).children("span").html("朕也是看報紙才知道")
+        }
+    })
+    jQuery(".uiLinkButtonInput").each(function() {
+        if (this.value == "Comment" || this.value == "留言") {
+            this.value = "卿家可知？"
+        }
+    })
     jQuery(".share_action_link").each(function() {
         if ($(this).text() == "Share" || $(this).text() == "分享") {
-            $(this).text("宣旨");
+            $(this).text("宣旨")
         }
-    });
+    })
 })
 
 observer.observe(document, {
